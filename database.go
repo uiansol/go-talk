99abc57b95411e2ba326e88b76faac2e8858fd11
Uian Sol <sol.uian@gmail.com>
2021-05-04 10:06:30 -0300

    User and Message models to database

--- database.go
+++ database.go
@@ -7,3 +7,17 @@ import (
 type Config struct {
 	DB *gorm.DB
 }
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"uniqueIndex"`
+	Password string
+	Messages []Message `gorm:"foreignKey:UserName"`
+}
+
+type Message struct {
+	ID        uint   `json:"-"`
+	UserName  string `json:"user_name"`
+	Text      string `json:"text"`
+	CreatedAt int64  `json:"created_at" gorm:"index,autoCreateTime:milli"`
+}
