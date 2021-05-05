71ada9905e61872005650c195f144ba1bc299d96
Uian Sol <sol.uian@gmail.com>
2021-05-05 10:35:48 -0300

    Add create user and login handlers

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -33,4 +34,51 @@ func (c *Config) SetupRoutes() *chi.Mux {
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
+}
+
+func (c *Config) NewUserPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	userName := r.PostForm.Get("user")
+	userPassword := r.PostForm.Get("password")
+	userPasswordConfirm := r.PostForm.Get("password_confirm")
+
+	if userName == "" || userPassword == "" {
+		http.Error(w, "missing user or password", http.StatusBadRequest)
+		return
+	}
+
+	if userPassword != userPasswordConfirm {
+		http.Error(w, "passwords do not match", http.StatusBadRequest)
+		return
+	}
+
+	if _, err := c.CreateUser(userName, userPassword); err != nil {
+		log.Printf("database query error: %q\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (c *Config) LoginPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	userName := r.PostForm.Get("user")
+	userPassword := r.PostForm.Get("password")
+
+	if userName == "" || userPassword == "" {
+		http.Error(w, "missing user or password", http.StatusBadRequest)
+		return
+	}
+
+	if err := c.CheckLogin(userName, userPassword); err != nil {
+		http.Error(w, "login unsuccessful", http.StatusBadRequest)
+		return
+	}
+
+	token := c.MakeToken(userName)
+	http.SetCookie(w, &http.Cookie{
+		Name:  "jwt",
+		Value: token,
+	})
+	http.Redirect(w, r, "/chat", http.StatusSeeOther)
 }
